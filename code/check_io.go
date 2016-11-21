751716c973fdd7b9af032412df2a94523f9a2f15
daguanr <daguanr@gmail.com>
2016-11-21 21:31:38 +0800

    代码完成check_io

--- code/check_io.go
+++ code/check_io.go
@@ -11,6 +11,8 @@ import (
 	"github.com/shirou/gopsutil/disk"
 )
 
+const MSG = "[%s]_IOStatus: %s - read(KB)/s=%d, write(KB)/s=%d | read=%d;%d;0;0; write=%d;%d;0;0;"
+
 func main() {
 	argv := os.Args
 	if len(argv) != 6 {
@@ -25,13 +27,21 @@ func main() {
 	v, _ := disk.IOCounters()
 	time.Sleep(1000)
 	v2, _ := disk.IOCounters()
-	rs := (v2[d].ReadBytes - v[d].ReadBytes) >> 10
-	ws := (v2[d].WriteBytes - v[d].WriteBytes) >> 10
+	rs := int((v2[d].ReadBytes - v[d].ReadBytes)) >> 10
+	ws := int((v2[d].WriteBytes - v[d].WriteBytes)) >> 10
 	var sta string
 	var exit_code int
 	if (rs > rw && rs < rc) || (ws > ww && ws < wc) {
-		sta = "w"
+		sta = "WARNING"
 		exit_code = 1
+	} else if rs > rc || ws > wc {
+		sta = "Critical"
+		exit_code = 2
+	} else {
+		sta = "OK"
+		exit_code = 0
 	}
+	fmt.Printf(MSG, d, sta, rs, ws, rw, rc, ww, wc)
+	os.Exit(exit_code)
 
 }
